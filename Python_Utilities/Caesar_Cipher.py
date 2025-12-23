9bdda7a7523bd7efc9036fad1905fc96318a7be3
naisha2080 <214511670+naisha2080@users.noreply.github.com>
2025-12-23 13:56:41 +0530

    Caesar Cipher added

---
+++ Python_Utilities/Caesar_Cipher.py
@@ -0,0 +1,50 @@
+"""
+Docstring for Python_Utilities.Caesar_Cipher
+
+Create a Python script that helps you send secret message to your friend using simple encryption.
+
+Ask the user if they want to encrypt or decrypt a message.
+If encrypt:
+- Ask for a message and a numeric sceret key
+- Use a Caesar Cipher (shift letters by the key value).
+- Output the encrypted message.
+If decrypt:
+- Ask for the encrypted message and key
+- Reverse the encryption to get the original message.
+
+letter → number → shift → wrap → letter again
+"""
+def encrypt(message, key):
+    result = ""
+    for char in message:
+        base = ord('A') if char.isupper() else ord('a')
+        shifted = (ord(char) - base + key) % 26 + base
+        result += chr(shifted)
+    return result
+
+
+def decrypt(message, key):
+    return encrypt(message, -key)
+
+
+print("Secret message program")
+choice = input("Do you want to E(Encrypt) or D(Decrypt) ? ").strip().lower()
+
+if choice == "e":
+    text = input("Enter your encrypted message: \n")
+    try:
+        key = int(input("Enter a number between 1-26 "))
+        encrypted = encrypt(text, key)
+        print(encrypted)
+    except ValueError:
+        print("Choose a valid key")
+elif choice == "d":
+    text = input("Enter your decrypted message: \n")
+    try:
+        key = int(input("Enter a number between 1-26 "))
+        decrypted = decrypt(text, key)
+        print(decrypted)
+    except ValueError:
+        print("Choose a valid key")
+else:
+    print("Invalid choice")
